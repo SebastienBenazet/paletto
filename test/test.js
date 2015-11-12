c247fcbe280f77d6d07aa08a5ecae4553c434da7
Sébastien Bénazet <s.benazet.59@gmail.com>
2015-11-12 10:46:45 +0100

    Test History 3 written. Refactoring of history 1 and 2

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ PalettoTestCase.prototype.testStory1 = function () {
     var moteur = new Engine();
     moteur.new_game();
 
-    assertTrue(moteur.juxtaposition_is_ok() === true);
+    assertTrue(moteur.juxtaposition_is_ok());
 };
 
 PalettoTestCase.prototype.testStory2 = function () {
@@ -15,7 +15,23 @@ PalettoTestCase.prototype.testStory2 = function () {
     var moteur = new Engine();
     moteur.new_game();
 
-    assertTrue(moteur.juxtaposition_is_ok() === true);
+    assertTrue(moteur.juxtaposition_is_ok());
 
-    assertTrue(moteur.in_corner("jaune") === true);
+    assertTrue(moteur.in_corner("jaune"));
+};
+
+PalettoTestCase.prototype.testStory3 = function () {
+    "use strict";
+    var moteur = new Engine();
+    moteur.new_game();
+
+    assertTrue(moteur.juxtaposition_is_ok());
+
+    assertTrue(moteur.in_corner("jaune"));
+
+    moteur.remove_piece("A6");
+    
+    assertTrue(moteur.get_number_piece() === 35);
+
+    assertTrue(moteur.get_number_piece_player(1,"jaune") == 1)
 };

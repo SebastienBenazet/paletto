706a92cf8292b8c1b752ffd733fbbb585fe72a8b
Sébastien Bénazet <s.benazet.59@gmail.com>
2015-11-12 10:26:53 +0100

    Test history 2 written

--- test/test.js
+++ test/test.js
@@ -9,3 +9,13 @@ PalettoTestCase.prototype.testStory1 = function () {
 
     assertTrue(moteur.juxtaposition_is_ok() === true);
 };
+
+PalettoTestCase.prototype.testStory2 = function () {
+    "use strict";
+    var moteur = new Engine();
+    moteur.new_game();
+
+    assertTrue(moteur.juxtaposition_is_ok() === true);
+
+    assertTrue(moteur.in_corner("jaune") === true);
+};
